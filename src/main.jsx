d4f507d106b2342bdf096f754fc5b7014cd845b4
Jojobusiness <johnfres.45@gmail.com>
2025-06-23 00:09:29 +0200

    fix: remove @vercel/analytics to fix build

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './Home.jsx';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home />
+    <Analytics/>
+    <SpeedInsights/>
   </React.StrictMode>
 );
