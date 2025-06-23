c28f3bf969d916f42028b2169470042bc549dc57
Jojobusiness <johnfres.45@gmail.com>
2025-06-23 03:23:23 +0200

    fix: Rajout des pages et des fonctions

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
-import Home from './Home.jsx';
+import Home from './pages/Home';
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css';
