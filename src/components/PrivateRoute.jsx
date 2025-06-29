16f9a46cd1f3698103b5e834d5c59773f78c3117
Jojobusiness <johnfres.45@gmail.com>
2025-06-29 19:10:45 +0200

    fix: Affichage des clients et Factures

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,8 @@
-//import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/AuthContext';
 import { Navigate } from 'react-router-dom';
 
 export default function PrivateRoute({ children }) {
-  // const { user, loading } = useAuth();
-  const user = true;
-  const loading = false;
+  const { user, loading } = useAuth();
 
   if (loading) return <p>Chargement...</p>;
   return user ? children : <Navigate to="/login" />;
