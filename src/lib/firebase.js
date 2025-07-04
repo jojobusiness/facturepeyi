0b6781dd8985874a76aa643bce4b2e3130b73b33
Jojobusiness <johnfres.45@gmail.com>
2025-07-04 22:16:39 +0200

    fix: Parametre avancer

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,3 +23,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 const db = getFirestore(app);
 export { db };
+export const storage = getStorage(app);
