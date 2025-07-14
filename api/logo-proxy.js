380f529a054935bd11c6910c29b6440dd2abb2c4
Jojobusiness <johnfres.45@gmail.com>
2025-07-14 03:04:10 +0200

    fix: update générale

--- api/logo-proxy.js
+++ api/logo-proxy.js
@@ -1,25 +1,15 @@
 export default async function handler(req, res) {
   const { url } = req.query;
-  if (!url) return res.status(400).send("URL manquante");
+  if (!url) return res.status(400).send("Missing image URL");
 
   try {
     const response = await fetch(url);
-
-    if (!response.ok) {
-      const text = await response.text();
-      console.error("⚠️ Erreur Firebase :", response.status, text);
-      return res.status(500).send("Erreur téléchargement image");
-    }
-
     const buffer = await response.arrayBuffer();
-    const contentType = response.headers.get("content-type");
     const base64 = Buffer.from(buffer).toString("base64");
-
-    const dataUrl = `data:${contentType};base64,${base64}`;
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).send(dataUrl);
-  } catch (err) {
-    console.error("❌ Erreur proxy générale :", err);
+    const contentType = response.headers.get("content-type");
+    res.status(200).send(`data:${contentType};base64,${base64}`);
+  } catch (error) {
+    console.error("Erreur proxy:", error);
     res.status(500).send("Erreur proxy");
   }
 }
